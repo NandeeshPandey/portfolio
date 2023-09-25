5e54f4479bf9c34178831a8dbf0196be80c31e97
NandeeshPandey <113363736+NandeeshPandey@users.noreply.github.com>
2023-09-25 19:19:36 +0530

    Add files via upload

---
+++ types.d.ts
@@ -0,0 +1,13 @@
+export interface Testimonial {
+  _id: string;
+  name: string;
+  content: string;
+  picture: {};
+  position: string;
+}
+
+export interface Skill {
+  _id: string;
+  title: string;
+  image: {};
+}
