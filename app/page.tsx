813ad028c98170b339fa846799d9a476287fb503
Darkcodelab <deepanchakravarthi434@gmail.com>
2023-10-15 18:20:25 +0530

    Added language page

--- app/page.tsx
+++ app/page.tsx
@@ -10,6 +10,7 @@ import Footer from "@/components/Footer";
 // actions
 import { getSkills, getTestimonials } from "@/sanity/action";
 import { Skill } from "@/types";
+import CallToAction from "@/components/CallToAction";
 
 export const revalidate = 900;
 
@@ -86,22 +87,7 @@ export default async function Home() {
         </section>
 
         {/* cta section */}
-        <section className="mt-20 relative py-10 p-4">
-          <div className="absolute bottom-0 h-[100px] bg-primary w-full z-10 left-0"></div>
-          <div className="relative bg-[#141C3A] z-20 max-w-[900px] w-full mx-auto text-white p-4 py-10 rounded-xl flex items-center justify-around gap-6 flex-col lg:flex-row">
-            <h2 className="font-bold text-2xl">Start a project</h2>
-            <p className="max-w-[300px] text-sm">
-              Interested in working together? We should queue up a time to chat.
-              I&apos;ll buy the coffee.
-            </p>
-            <Link
-              href="/contact"
-              className="p-2 border-2 rounded-full border-accent text-accent max-w-[150px] w-full text-center transition-all hover:bg-accent hover:text-white"
-            >
-              Let&apos;s do this
-            </Link>
-          </div>
-        </section>
+        <CallToAction />
       </main>
       <Footer />
     </>
